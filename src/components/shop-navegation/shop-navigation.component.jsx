6930e4edf099b8618413e79dd4d9796330b422d7
Jonathanbir <jonathan79111@gmail.com>
2022-07-27 15:25:14 +0800

    commit edit cart dropdown

--- src/components/shop-navegation/shop-navigation.component.jsx
+++ src/components/shop-navegation/shop-navigation.component.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import { Fragment, useState, useContext } from "react";
+import { Fragment, useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useScroll } from "../../utils/hook/useScroll";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import Navigation from "../../routes/navigation/navigation.component";
-import { CartContext } from "../../contexts/cart.context";
-
+import { selectCurrentUser } from "../../store/user/user.selector";
+import { selectIsCartOpen } from "../../store/cart/cart.selector";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import { motion } from "framer-motion";
 import "./shop-navigation.styles.scss";
 
 const ShopNavigation = () => {
-  const currentUser = useSelector((state) => state.user.currentUser);
-  // const { currentUser } = useContext(UserContext);
-  const { isCartOpen } = useContext(CartContext);
+  const currentUser = useSelector(selectCurrentUser);
+  const isCartOpen = useSelector(selectIsCartOpen);
   const [open, setOpen] = useState(false);
   const animateFrom = { x: -100 };
   const animateTo = { x: 0 };
